b983c9be3578e974419caf8631f828e9877c6c0d
mdev1987 <m.developer66@gmail.com>
2023-03-12 01:54:13 +0330

    Install NextAuth and added authentication with google

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,5 +1,44 @@
+import { signIn, signOut, useSession } from "next-auth/react"
+
 export default function User() {
+    const { data: session } = useSession()
+    if (session) {
+        if (session.user?.image) {
+            return (
+                <>
+                    <img className="h-10 w-10 
+                        rounded-full
+                      hover:bg-gray-200 
+                        cursor-pointer p-1"
+                        onClick={() => signOut()}
+                        src={session.user?.image || ""} alt="user image" />
+                </>
+            )
+        }
+        const name = session.user?.name?.split(" ")
+            .map(item => item.charAt(0))
+            .join("").toUpperCase()
+        return (
+            <>
+                <div onClick={() => signOut()}
+                    className="flex justify-center items-center
+                    bg-blue-800
+                    text-white h-10 w-10 
+                      rounded-full hover:brightness-105 
+                      hover:shadow-md cursor-pointer p-1">
+                    <span>{name}</span>
+                </div>
+            </>
+        )
+
+    }
     return (
-        <div>User</div>
+        <>
+            <button className="bg-blue-500 text-white 
+                                rounded-md px-6 py-2 font-medium 
+                                hover:brightness-105 
+                                hover:shadow-md"
+                onClick={() => signIn()}>Sign in</button>
+        </>
     )
 }
---
+++ src/pages/auth/signin.tsx
@@ -0,0 +1,38 @@
+import Header from "@/components/Header";
+import { BuiltInProviderType } from "next-auth/providers";
+import { ClientSafeProvider, getProviders, LiteralUnion, signIn } from "next-auth/react"
+import Image from "next/image";
+import google from '../../../public/assets/images/google.png'
+
+export default function signin({ providers }: { providers: Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> }) {
+    console.dir(providers);
+    return (
+        <>
+            <Header />
+            <div className="mt-36">
+                {Object.values(providers).map(provider => (
+                    <div className="flex flex-col items-center" key={provider.id}>
+                        <Image
+                            className="w-52 object-cover"
+                            src={google}
+                            placeholder={'blur'}
+                            alt="google" />
+                        <p className="text-sm italic my-10 text-center">This website is created for study case</p>
+                        <button className="bg-red-500 rounded-lg text-white p-3 hover:bg-red-600"
+                            onClick={() => signIn(provider.id, { callbackUrl: '/' })}>Sign in with {provider.name}</button>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+
+export async function getServerSideProps() {
+    const providers = await getProviders();
+    return {
+        props: {
+            providers
+        }
+    }
+}
