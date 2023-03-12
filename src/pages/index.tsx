d42e48abfe749b7948dc55bab590666ffb983637
mdev1987 <m.developer66@gmail.com>
2023-03-12 04:34:27 +0330

    Completed body section in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import Header from "@/components/Header";
 import Head from "next/head";
+import Image from "next/image";
+import { MagnifyingGlassIcon, MicrophoneIcon } from '@heroicons/react/20/solid';
+import google from '../../public/assets/images/google.png'
 
 export default function Home() {
   return (
@@ -14,7 +17,26 @@ export default function Home() {
       <Header />
 
       {/* Body */}
-
+      <form className="flex flex-col items-center mt-20" >
+        <Image src={google}
+          alt="google"
+          className="object-cover"
+          placeholder={"blur"}
+          width={300} height={100} />
+        <div className="flex w-full mt-5 mx-auto 
+          max-w-[90%] border border-gray-200
+          hover:shadow-lg focus-within:shadow-lg
+          px-5 py-3 rounded-full items-center 
+          sm:max-w-xl lg:max-w-2xl">
+          <MagnifyingGlassIcon className="h-5 text-gray-500 mr-3" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+        <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
+          <button className="btn">Google Search</button>
+          <button className="btn">I'm Feeling Lucky</button>
+        </div>
+      </form>
       {/* Footer */}
     </div>
   )
