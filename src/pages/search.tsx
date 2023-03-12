9a4d0fa433cd20bae313b0498be8cc2d410ad381
mdev1987 <m.developer66@gmail.com>
2023-03-13 01:56:50 +0330

    Get data from google api and create the mock data

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,9 +1,9 @@
 import SearchHeader from "@/components/SearchHeader";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
+import mockResponse from '../mock-response.json'
 
-export default function search() {
-
+export default function search({ data, error }: { data: any, error?: string }) {
     return (
         <>
             <Head>
@@ -16,4 +16,37 @@ export default function search() {
             {/* Search Result */}
         </>
     )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const API_KEY = process.env.GOOGLE_SEARCH_ENGINE_API_KEY || ''
+    const ENGINE_ID = process.env.GOOGLE_SEARCH_ENGINE_ID || ''
+    const term = ctx.query.term;
+    const searchType = (ctx.query?.searchType || '') === 'images' ? '&searchType=image' : '';
+    const mockData = true;
+    if (mockData) {
+        return {
+            props: {
+                error: false,
+                data: mockResponse
+            }
+        }
+    }
+    const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${ENGINE_ID}&q=${term}${searchType}`,
+        { method: 'GET' })
+    if (response.ok) {
+        return {
+            props: {
+                data: await response.json(),
+                error: false
+            }
+        }
+    }
+    return {
+        props: {
+            data: null,
+            error: `Error: ${response.status}: ${response.statusText}`
+        }
+    }
 }
